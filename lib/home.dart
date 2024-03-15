59d6a6e484e63aab40ea8c7b3120f1a148ea9753
MatheusAmaralDeSousa <theusamarausyt@gmail.com>
2024-03-14 21:53:39 -0300

    refactor: home, main and dependencies

--- lib/home.dart
+++ lib/home.dart
@@ -1,47 +1,43 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-  
+
   @override
   State<HomePage> createState() => _HomePageState();
-
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   bool check = false;
   var endereco;
   var latitude;
   var longitude;
 
-  void _Check() async{
+  void _Check()async{
     await Geolocator.checkPermission();
     await Geolocator.requestPermission();
 
     Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high
-    );
-    latitude = position.latitude;
-    longitude = position.longitude;
-    List<Placemark>placemark = 
-      await placemarkFromCoordinates(latitude, longitude);
-    Placemark place = placemark[0];
-   setState(() {
-     check = true;
-     endereco = "${place.locality}, ${place.postalCode}, ${place.country}";
-   }); 
+      desiredAccuracy: LocationAccuracy.high);
+
+      latitude = position.latitude;
+      longitude = position.longitude;
+      List<Placemark>placemark = await placemarkFromCoordinates(latitude, longitude);
+      Placemark place = placemark[0];
+
+      setState(() {
+        check = true;
+        endereco = "${place.locality}, ${place.postalCode}, ${place.country}";
+      });
   }
 
   @override
-  Widget build(BuildContext){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AppGPS"),
+        title: Text("App GPS"),
         centerTitle: true,
       ),
       body: Center(
@@ -49,14 +45,18 @@ class _HomePageState extends State<HomePage>{
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             check ? Text(endereco) : Container(),
-            check ? Text('Latiturde = $latitude') : Container(),
+            check ? Text('Latitude = $latitude') : Container(),
             check ? Text('Longitude = $longitude') : Container(),
-            TextButton(onPressed: () {
-              _Check();
-            }, child: Text("Localiza ai bb", 
-            style: TextStyle(fontSize: 20),))
+            TextButton(
+              onPressed: () {
+                _Check();
+              }, 
+              child: 
+              Text("Localiza ai", style: TextStyle(fontSize: 20),
+              ))
           ],
-        ),),
+        ),
+      ),
     );
   }
 }
